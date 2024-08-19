47f7f92cb3439dfbfa54813e648f0fa2fb95d0d0
stugnait <teslitskii.link@gmail.com>
2024-08-20 00:41:06 +0300

    first task completed

---
+++ variables.ts
@@ -0,0 +1,12 @@
+let stringVar: string= "text";
+let intVar: number= 3;
+let isVisible: boolean=true;
+let anyVar: any = "123";
+
+console.log(stringVar);
+console.log(intVar);
+console.log(isVisible);
+console.log(anyVar);
+
+let intArray: number[] = [1,3,4,5,2]
+let stringArray: string[] = ["try","to","catch","me",]
